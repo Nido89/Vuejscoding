611b22b1fc8bbbf6176fa48c7530985a5d5fa3cc
Naveed <naveed.jalal@edu.novia.fi>
2020-03-24 08:26:51 +0000

    A photo animation with sass-loader and vuex-router

---
+++ Appstorephoto/appstorephotos/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="scss">
+* {
+  font-size: 1rem;
+}
+body {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  font-family: "Segoe UI", Tahoma;
+  background-color: #efefef;
+}
+#app {
+  position: relative;
+  width: calc(100% - 40px);
+  min-height: calc(100vh - 40px);
+  padding: 20px;
+  color: #333;
+  overflow: hidden;
+}
+</style>
---
+++ Vuejsdatabindings/databinding/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <h1>{{title}}</h1>
+    <input type="text" v-on: input="changeTitle">
+</div>
+  
+</template>
+
+<script>
+export default {
+  data:() => {
+    return {
+      title:'Good Morning'
+    }
+  },
+  methods: {
+    changeTitle(event){
+      this.title =event.data.changeTitle;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
